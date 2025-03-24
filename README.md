735c11d918e8b5cb9c01ccf9a3280e0a80668b7e
Mike Kistler <mikekistler@microsoft.com>
2025-03-24 07:56:02 -0700

    Demo preview1 features

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # mvpsummit-openapi-demo
+
 OpenAPI demo for MVP Summit 2025
+
+Features already available in .NET Previews:
+
+- Support for generating OpenAPI 3.1
+  - Leveraging OpenAPI.NET 2.0.0 (new major version with breaking changes)
+
+- Generation of OpenAPI in YAML format
+
+- Support for response descriptions
+  - New "Description" parameter on [ProducesResponseType] attribute
+
+- Support for OpenAPI metadata in XML doc comments
+
+
--- openapi-demo/Program.cs
+++ openapi-demo/Program.cs
@@ -1,15 +1,51 @@
+using System.Text.Json.Nodes;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options =>
+{
+    // Specify the OpenAPI version to use.
+    // options.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi3_0;
+    // Here's how to add an example in .NET 9
+    // options.AddSchemaTransformer((schema, context, cancellationToken) =>
+    // {
+    //     if (context.JsonTypeInfo.Type == typeof(WeatherForecast))
+    //     {
+    //         schema.Example = new OpenApiObject
+    //         {
+    //             ["date"] = new OpenApiString(DateTime.Now.AddDays(1).ToString("yyyy-MM-dd")),
+    //             ["temperatureC"] = new OpenApiInteger(0),
+    //             ["temperatureF"] = new OpenApiInteger(32),
+    //             ["summary"] = new OpenApiString("Bracing"),
+    //         };
+    //     }
+    //     return Task.CompletedTask;
+    // });
+    options.AddSchemaTransformer((schema, context, cancellationToken) =>
+    {
+        if (context.JsonTypeInfo.Type == typeof(WeatherForecast))
+        {
+            schema.Example = new JsonObject
+            {
+                ["date"] = DateTime.Now.AddDays(1).ToString("yyyy-MM-dd"),
+                ["temperatureC"] = 0,
+                ["temperatureF"] = 32,
+                ["summary"] = "Bracing",
+            };
+        }
+        return Task.CompletedTask;
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapOpenApi("/openapi/{documentName}.yaml");
 }
 
 app.UseHttpsRedirection();
@@ -19,7 +55,9 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast",
+[ProducesResponseType<ProblemDetails>(StatusCodes.Status400BadRequest, Description = "An unexpected error occurred")]
+() =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
